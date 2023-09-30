69498a1724d9002c99e26b6911857e2f0d38ca62
masaya.shimizu <masaya.shimizu@274.local>
2023-09-30 15:29:46 +0900

    そうさくんファイル

---
+++ SampleUI/SousaVC.swift
@@ -0,0 +1,24 @@
+//
+//  SousaVC.swift
+//  SampleUI
+//
+//  Created by masaya.shimizu on 2023/09/30.
+//
+
+import Foundation
+import UIKit
+
+class SousaVC: UIViewController {
+
+
+    //Viewがセットされたとき
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //そうさくん用ファイル
+        print("viewDidLoad")
+
+    }
+
+}
+
+
